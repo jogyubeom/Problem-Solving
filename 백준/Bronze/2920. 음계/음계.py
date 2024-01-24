e048d760a24b88feab4404b2b6672245feae7568
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-01-24 20:27:41 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+[c, d, e, f, g, a, b, C] = [1, 2, 3, 4, 5, 6, 7, 8]
+melody = input()
+if melody == '1 2 3 4 5 6 7 8':
+    print('ascending')
+elif melody == '8 7 6 5 4 3 2 1':
+    print('descending')
+else:
+    print('mixed')
