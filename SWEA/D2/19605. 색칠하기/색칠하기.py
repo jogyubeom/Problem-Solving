cc4818851671186147a9f1ec1981c6e16b77fce8
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-02-01 19:52:07 +0900

    [D2] Title: 색칠하기, Time: 149 ms, Memory: 58,516 KB -BaekjoonHub

--- SWEA/D2/19605. 색칠하기/색칠하기.py
+++ SWEA/D2/19605. 색칠하기/색칠하기.py
@@ -1,20 +1,22 @@
 T = int(input())
 for tc in range(1, T+1):
-    red_box = set()
-    blue_box = set()
+    arr = [[0] * 10 for _ in range(10)]
     N = int(input())
     for n in range(N):
         r1, c1, r2, c2, color = map(int,input().split())
-        color_box = set()
-        for r in range(r1, r2 + 1):
-            for c in range(c1, c2 + 1):
-                color_box.add((r,c))
-        if color == 1 :
-            red_box.update(color_box)
+        if color == 1:
+            for r in range(r1, r2 + 1):
+                for c in range(c1, c2 + 1):
+                    if arr[r][c] == 0 or arr[r][c] == 2:
+                        arr[r][c] += 1
         else:
-            blue_box.update(color_box)
+            for r in range(r1, r2 + 1):
+                for c in range(c1, c2 + 1):
+                    if arr[r][c] == 0 or arr[r][c] == 1:
+                        arr[r][c] += 2
     count = 0
-    for box in red_box:
-        if box in blue_box:
-            count += 1
+    for r in range(10):
+        for c in range(10):
+            if arr[r][c] == 3:
+                count += 1
     print(f'#{tc} {count}')
