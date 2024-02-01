0112046ac6ea04c8d40580f50c827cb1f644a1fc
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-02-02 01:54:46 +0900

    [D2] Title: 풍선팡2, Time: 189 ms, Memory: 62,804 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -0,0 +1,18 @@
+T = int(input())
+for tc in range(T):
+    N, M = map(int, input().split())    # N = 행 개수 , M = 풍선 개수 = 열 개수
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    dr = [0, 1, 0, -1]  # 우 하 좌 상
+    dc = [1, 0, -1, 0]
+    flower_spread = []
+    for r in range(N):
+        for c in range(M):
+            sum = arr[r][c]
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
+                if 0 <= nr <= N-1 and 0 <= nc <= M-1:
+                    sum += arr[nr][nc]
+            flower_spread.append(sum)
+    result = max(flower_spread)
+    print(f'#{tc+1} {result}')
