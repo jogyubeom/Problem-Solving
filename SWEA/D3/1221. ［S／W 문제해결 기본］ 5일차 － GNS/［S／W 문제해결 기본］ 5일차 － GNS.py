a7138dec327f70f61e007992396f0faf5b8c6b89
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-02-05 21:34:22 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 255 ms, Memory: 64,624 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -3,37 +3,14 @@ for tc in range(T):
     case, length = input().split()
     arr = input().split()
     order = [0] * 10
-    for n in arr:
-        if n == 'ZRO':
-            order[0] += 1
-        elif n == 'ONE':
-            order[1] += 1
-        elif n == 'TWO':
-            order[2] += 1
-        elif n == 'THR':
-            order[3] += 1
-        elif n == 'FOR':
-            order[4] += 1
-        elif n == 'FIV':
-            order[5] += 1
-        elif n == 'SIX':
-            order[6] += 1
-        elif n == 'SVN':
-            order[7] += 1
-        elif n == 'EGT':
-            order[8] += 1
-        elif n == 'NIN':
-            order[9] += 1
-    result = ['ZRO'] * order[0] +\
-    ['ONE'] * order[1] +\
-    ['TWO'] * order[2] +\
-    ['THR'] * order[3] +\
-    ['FOR'] * order[4] +\
-    ['FIV'] * order[5] +\
-    ['SIX'] * order[6] +\
-    ['SVN'] * order[7] +\
-    ['EGT'] * order[8] +\
-    ['NIN'] * order[9]
+    list = ['ZRO','ONE','TWO','THR','FOR','FIV','SIX','SVN','EGT','NIN']
+    for n in range(int(length)):
+        for i in range(10):
+            if arr[n] == list[i]:
+                order[i] += 1
+    result = []
+    for n in range(10):
+        result += [list[n]] * order[n]
 
     print(f'{case}')
     print(*result)
