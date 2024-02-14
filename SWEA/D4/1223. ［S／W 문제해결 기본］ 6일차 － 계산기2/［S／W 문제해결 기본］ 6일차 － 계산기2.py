f89b1020e732ec04c0f1f2f7feb9912a0c68b002
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-02-14 16:20:08 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 108 ms, Memory: 42,728 KB -BaekjoonHub

---
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -0,0 +1,44 @@
+for tc in range(1, 11):
+    N = int(input())    # 계산식 길이
+    code = input()
+    post_code = ''
+    top = -1
+    stack = ['0'] * N
+    for n in range(N):
+        if code[n] == '+':
+            if stack[top] in '+*':
+                while stack[top] in '+*':
+                    top -= 1
+                    post_code += stack[top + 1]
+            top += 1
+            stack[top] = code[n]
+        elif code[n] == '*':
+            if stack[top] == '*':
+                while stack[top] == '*':
+                    top -= 1
+                    post_code += stack[top + 1]
+            top += 1
+            stack[top] = code[n]
+        else:
+            post_code += code[n]
+        if n == N-1 and top != -1:
+            while top != -1:
+                top -= 1
+                post_code += stack[top + 1]
+
+    top = -1
+    stack = [0] * N
+    for n in post_code:
+        if n not in '+*':
+            top += 1
+            stack[top] = int(n)
+        elif n == '+':
+            val = stack[top-1] + stack[top]
+            top -= 1
+            stack[top] = val
+        elif n == '*':
+            val = stack[top-1] * stack[top]
+            top -= 1
+            stack[top] = val
+    result = stack[top]
+    print(f'#{tc} {result}')
