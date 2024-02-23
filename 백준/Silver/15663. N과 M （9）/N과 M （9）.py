aa37d917b5cbb69702dae998cd360babbb5fa61c
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-02-23 17:34:01 +0900

    [Silver II] Title: N과 M (9), Time: 160 ms, Memory: 113576 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def pick(M):
+    if M == 0:
+        print(*result)
+        return
+    flag = 0
+    for n in range(N):
+        if index_record[n] == 0 and flag != arr[n]:
+            result.append(arr[n])
+            index_record[n] = 1
+            flag = arr[n]
+            pick(M-1)
+            result.pop()
+            index_record[n] = 0
+
+
+N, M = map(int, sys.stdin.readline().split())    # 자연수, 고를 개수
+arr = list(map(int, sys.stdin.readline().split()))   # N개의 자연수 리스트
+arr.sort()
+result = []
+index_record = [0] * N
+pick(M)
