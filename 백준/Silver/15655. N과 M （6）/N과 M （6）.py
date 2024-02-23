6bbd1fc0061c951a1acc588040f61205cb7a671f
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-02-23 15:52:10 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,21 @@
+def pick(M):
+    if M == 0:
+        print(*result)
+        return
+
+    for n in range(N):
+        if len(result) == 0:
+            result.append(arr[n])
+            pick(M-1)
+            result.pop()
+        elif arr[n] not in result and arr[n] > result[-1]:
+            result.append(arr[n])
+            pick(M-1)
+            result.pop()
+
+
+N, M = map(int, input().split())    # 자연수, 고를 개수
+arr = list(map(int, input().split()))   # N개의 자연수 리스트
+arr.sort()
+result = []
+pick(M)
