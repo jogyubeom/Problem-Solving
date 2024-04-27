cd7f0343d820ac1e86234b48ebbe1dc646ca8bc6
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-04-28 04:04:25 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 260 ms, Memory: 131604 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+N, K = map(int, input().split())    # 수빈 위치, 동생 위치
+max_n = 100000
+visited = [0] * (max_n + 1)
+min_count = 1e9
+method = 0
+que = deque()
+que.append([N, 0])
+while que:
+    n, cnt = que.popleft()
+    if cnt > min_count:
+        continue
+    if n == K:
+        min_count = min(min_count, cnt)
+        method += 1
+    if n-1 >= 0:
+        if visited[n-1] == 0 or visited[n-1] >= visited[n] + 1:
+            que.append([n-1, cnt+1])
+            visited[n-1] = visited[n] + 1
+    if n < K and n+1 <= max_n:
+        if visited[n+1] == 0 or visited[n+1] >= visited[n] + 1:
+            que.append([n+1, cnt+1])
+            visited[n+1] = visited[n] + 1
+    if n < K and n*2 <= max_n:
+        if visited[n*2] == 0 or visited[n*2] >= visited[n] + 1:
+            que.append([n*2, cnt+1])
+            visited[n*2] = visited[n] + 1
+print(min_count)
+print(method)
