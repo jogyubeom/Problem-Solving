38e6dcb30eaa4f02800f5993b12ac9ef9b9cf17f
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-08-15 03:37:38 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+
+word = input()
+if word == 'SONGDO':
+    print('HIGHSCHOOL')
+elif word == 'CODE':
+    print('MASTER')
+elif word == '2023':
+    print('0611')
+elif word == 'ALGORITHM':
+    print('CONTEST')
