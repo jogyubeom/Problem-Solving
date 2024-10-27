bf720405678d47c23eea87e88001a64d5448b49d
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2024-10-28 02:43:30 +0900

    [Bronze III] Title: 부호, Time: 280 ms, Memory: 112096 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,11 @@
+for _ in range(3):
+    N = int(input())
+    S = 0
+    for i in range(N):
+        S += int(input())
+    if S == 0:
+        print(0)
+    elif S < 0:
+        print('-')
+    else:
+        print('+')
