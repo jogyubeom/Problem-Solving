b1386959afc54720ce925201fa637c1ad3432310
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2025-05-27 01:12:19 +0900

    [Silver I] Title: IOIOI, Time: 2452 ms, Memory: 209044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,21 +5,23 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 S = input()
+temp = 'I' + 'OI' * N
 count = 0
+n = 0
 
-def check(n):
-    for i in range(2 * N + 1):
-        if i % 2 == 0:
-            if S[n + i] != 'I':
-                return False
-        else:
-            if S[n + i] != 'O':
-                return False
-    
-    return True
-
-for n in range(M - 2 * N):
-    if check(n):
+while n < M - 2 * N:
+    if S[n] != 'I':
+        n += 1
+    elif S[n:n + 2 * N + 1] == temp:
         count += 1
+        while True:
+            if S[n + 2 * N + 1:n + 2 * N + 3] == 'OI':
+                count += 1
+                n += 2
+            else:
+                break
+        n += 2 * N + 1
+    else:
+        n += 1
 
 print(count)
