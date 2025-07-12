e49ce102ace9efa81d37aee44f5fd73f09fd792a
jogyubeom <156288284+jogyubeom@users.noreply.github.com>
2025-07-13 00:35:38 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,6 @@
+name = dict()
+name['NLCS'] = 'North London Collegiate School'
+name['BHA'] = 'Branksome Hall Asia'
+name['KIS'] = 'Korea International School'
+name['SJA'] = 'St. Johnsbury Academy'
+print(name[input()])
